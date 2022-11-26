eb6731b46b61a29c8e566953e056e8a150a0797d
Tais Calisto <taiscalisto97@gmail.com>
2022-11-26 18:54:04 -0300

    fixed header detail

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -17,7 +17,7 @@ export const HeaderStyle = styled.header`
     }
   }
   .background-detail {
-    position: absolute;
+    position: fixed;
     width: 16rem;
     height: 16rem;
     left: -50px;
